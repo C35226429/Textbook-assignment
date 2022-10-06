386115e5f3581a6c093dda80a308eb233b7ec099
C35226429 <114905139+C35226429@users.noreply.github.com>
2022-10-06 13:41:58 +0800

    Add files via upload

---
+++ while的嵌套循环.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	scanf("%d",&num);
+	int i,j,k;
+	i=num;
+	int cnt;
+	while(i<=num+3){
+		j=num;
+		while(j<=num+3){
+			k=num;
+			while(k<=num+3){
+				if(){
+					if(){
+						if(){
+							printf("");
+							cnt++;
+							if(cnt%6==0)
+							printf("\n");
+							else printf(" ");
+						}
+					}
+				}
+			k++;	
+			}
+		j++;
+		}
+	i++;	
+	}
+
+	
+}
